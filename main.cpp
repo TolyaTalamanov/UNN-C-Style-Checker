2c9fc24d7d51c38c69cb8b6ec219199b2458e75c
Кристина <miss-yurievna@ya.ru>
2020-05-15 15:14:04 +0300

    Complete c-style-checker task

--- main.cpp
+++ main.cpp
@@ -18,13 +18,41 @@ using namespace clang::tooling;
 
 class CastCallBack : public MatchFinder::MatchCallback {
 public:
-    CastCallBack(Rewriter& rewriter) {
-        // Your code goes here
+    CastCallBack(Rewriter& rewriter) : rewriter_(rewriter) {
+
     };
 
     virtual void run(const MatchFinder::MatchResult &Result) {
-        // Your code goes here
+      if (const auto *Cast_Term = Result.Nodes.getNodeAs<CStyleCastExpr>("cast")) {
+        if (Cast_Term->getCastKind() == CK_ToVoid)
+          return;
+        if (Cast_Term->getExprLoc().isMacroID())
+          return;
+
+
+        auto Replace_Range = CharSourceRange::getCharRange(
+          Cast_Term->getLParenLoc(), Cast_Term->getSubExprAsWritten()->getBeginLoc());
+
+        StringRef String_Type = Lexer::getSourceText(CharSourceRange::getTokenRange(
+          Cast_Term->getLParenLoc().getLocWithOffset(1),
+          Cast_Term->getRParenLoc().getLocWithOffset(-1)),
+          *Result.SourceManager, Result.Context->getLangOpts());
+
+        std::string Type_Change(("static_cast<" + String_Type + ">").str());
+
+        const auto *Cast_Ignore = Cast_Term->getSubExprAsWritten()->IgnoreImpCasts();
+
+        if (!isa<ParenExpr>(Cast_Ignore)) {
+
+          Type_Change.push_back('(');
+          rewriter_.InsertText(Lexer::getLocForEndOfToken(Cast_Ignore->getEndLoc(),
+            0, *Result.SourceManager, Result.Context->getLangOpts()), ")");
+        }
+        rewriter_.ReplaceText(Replace_Range, Type_Change);
+      }
     }
+private:
+  Rewriter& rewriter_;
 };
 
 class MyASTConsumer : public ASTConsumer {
