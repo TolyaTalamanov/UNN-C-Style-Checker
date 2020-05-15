fcc965dcfde4158f267da2a9a0ef1ba41289c2ed
Guschin Alexandr <iguschin1999@gmail.com>
2020-05-15 17:20:54 +0300

    Complete c-style-checker task

--- main.cpp
+++ main.cpp
@@ -17,15 +17,40 @@ using namespace clang::ast_matchers;
 using namespace clang::tooling;
 
 class CastCallBack : public MatchFinder::MatchCallback {
-public:
-    CastCallBack(Rewriter& rewriter) {
-        // Your code goes here
-    };
+  private:
+    Rewriter& _rewriter;
+  public:
+    CastCallBack(Rewriter& rewriter) : _rewriter(rewriter) {};
 
-    virtual void run(const MatchFinder::MatchResult &Result) {
-        // Your code goes here
-    }
-};
+  virtual void run(const MatchFinder::MatchResult &Result) {
+    const auto* CastExpr = Result.Nodes.getNodeAs<CStyleCastExpr>("cast");
+  
+    if(CastExpr->getExprLoc().isMacroID())
+      return;
+    
+    if(CastExpr->getCastKind() == CK_ToVoid)
+      return;
+        
+    auto ReplaceRange = CharSourceRange::getCharRange(
+        CastExpr->getLParenLoc(), CastExpr->getSubExprAsWritten()->getBeginLoc());
+    
+    auto& SM = *Result.SourceManager;
+
+    const Expr *SubExpr = CastExpr->getSubExprAsWritten()->IgnoreImpCasts();
+
+    auto DestTypeString =
+      Lexer::getSourceText(CharSourceRange::getTokenRange(
+            CastExpr->getLParenLoc().getLocWithOffset(1),
+            CastExpr->getRParenLoc().getLocWithOffset(-1)),
+          SM, Result.Context->getLangOpts());
+
+    std::string replaceText = ("static_cast<"+ DestTypeString +">(").str();
+
+    _rewriter.ReplaceText(ReplaceRange, replaceText);
+    _rewriter.InsertText(Lexer::getLocForEndOfToken(SubExpr->getEndLoc(),
+			                                    0, SM, Result.Context->getLangOpts()), ")");
+  }
+}; 
 
 class MyASTConsumer : public ASTConsumer {
 public:
