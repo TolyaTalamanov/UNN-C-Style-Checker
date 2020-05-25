76d91772cc05efc96cd3433c240f3828b5011677
381706-2KDA2000 <dimentor2000@yandex.ru>
2020-05-25 14:32:18 +0300

    complete

--- main.cpp
+++ main.cpp
@@ -17,14 +17,44 @@ using namespace clang::ast_matchers;
 using namespace clang::tooling;
 
 class CastCallBack : public MatchFinder::MatchCallback {
+private:
+	Rewriter& _rewriter;
 public:
-    CastCallBack(Rewriter& rewriter) {
-        // Your code goes here
-    };
+    CastCallBack(Rewriter& rewriter) : _rewriter(rewriter) {};
 
     virtual void run(const MatchFinder::MatchResult &Result) {
         // Your code goes here
-    }
+		const auto* CastExpr = Result.Nodes.getNodeAs<CStyleCastExpr>("cast");
+
+		if (CastExpr->getExprLoc().isMacroID())
+			return;
+
+		if (CastExpr->getCastKind() == CK_ToVoid)
+			return;
+
+		const auto DestTypeAsWritten = CastExpr->getTypeAsWritten().getUnqualifiedType();
+		const auto SourceTypeAsWritten = CastExpr->getSubExprAsWritten()->getType().getUnqualifiedType();
+		const auto SourceType = SourceTypeAsWritten.getCanonicalType();
+		const auto DestType = DestTypeAsWritten.getCanonicalType();
+
+		auto ReplaceRange = CharSourceRange::getCharRange(
+			CastExpr->getLParenLoc(), CastExpr->getSubExprAsWritten()->getBeginLoc());
+
+		auto& SM = *Result.SourceManager;
+	
+		auto DestTypeString = Lexer::getSourceText(CharSourceRange::getTokenRange(
+			CastExpr->getLParenLoc().getLocWithOffset(1), CastExpr->getRParenLoc().getLocWithOffset(-1)),
+			SM, Result.Context->getLangOpts());
+
+		[&](std::string CastText) {
+			const Expr *SubExpr = CastExpr->getSubExprAsWritten()->IgnoreImpCasts();
+			if (!isa<ParenExpr>(SubExpr)) {
+				CastText.push_back('(');
+				_rewriter.InsertText(Lexer::getLocForEndOfToken(SubExpr->getEndLoc(), 0, SM, Result.Context->getLangOpts()),")");
+			}
+			_rewriter.ReplaceText(ReplaceRange, CastText);
+		}(("static_cast<" + DestTypeString + ">").str());
+	}
 };
 
 class MyASTConsumer : public ASTConsumer {
