aadb7d13612113625d0239fe768515002b4f6ed9
wappah <sokova.nastya9@gmail.com>
2020-05-26 18:57:34 +0300

    Complete c-style-checker

--- main.cpp
+++ main.cpp
@@ -3,28 +3,43 @@
 
 #include <llvm/Support/CommandLine.h>
 
-#include <clang/ASTMatchers/ASTMatchFinder.h>
-#include <clang/ASTMatchers/ASTMatchers.h>
-#include <clang/Basic/Diagnostic.h>
-#include <clang/Frontend/FrontendActions.h>
-#include <clang/Tooling/CommonOptionsParser.h>
-#include <clang/Tooling/Tooling.h>
-#include <clang/Rewrite/Core/Rewriter.h>
-#include <clang/Frontend/CompilerInstance.h>
-
-using namespace clang;
-using namespace clang::ast_matchers;
-using namespace clang::tooling;
-
 class CastCallBack : public MatchFinder::MatchCallback {
 public:
-    CastCallBack(Rewriter& rewriter) {
-        // Your code goes here
-    };
+    CastCallBack(Rewriter& rewriter) : _rewriter(rewriter) {};
 
     virtual void run(const MatchFinder::MatchResult &Result) {
-        // Your code goes here
+
+    const auto *CExp = Result.Nodes.getNodeAs<CStyleCastExpr>("cast");
+
+    SourceManager &SourMan = *Result.SourceManager;
+
+    auto ReplaceRange = CharSourceRange::getCharRange (
+		    CExp->getLParenLoc(), 
+		    CExp->getSubExprAsWritten()->getBeginLoc());
+
+    StringRef DestTypeString = Lexer::getSourceText(CharSourceRange::getTokenRange(
+    	CExp->getLParenLoc().getLocWithOffset(1), 
+    	CExp->getRParenLoc().getLocWithOffset(-1)),
+    	SourMan, Result.Context->getLangOpts());
+
+    const Expr *Expression = CExp->getSubExprAsWritten()->IgnoreImpCasts();
+
+    std::string CastReplaceText = ("static_cast<" + DestTypeString + ">").str();
+
+    if (!isa<ParenExpr>(Expression)){
+    	CastReplaceText.push_back('(');
+
+	_rewriter.InsertText(Lexer::getLocForEndOfToken(Expression->getEndLoc(),
+	0,
+	*Result.SourceManager,
+       	Result.Context->getLangOpts()),
+	")");
     }
+
+    _rewriter.ReplaceText(ReplaceRange, CastReplaceText);
+    }
+private:
+    Rewriter& _rewriter;
 };
 
 class MyASTConsumer : public ASTConsumer {
@@ -33,39 +48,15 @@ public:
         matcher_.addMatcher(
                 cStyleCastExpr(unless(isExpansionInSystemHeader())).bind("cast"), &callback_);
     }
-
     void HandleTranslationUnit(ASTContext &Context) override {
         matcher_.matchAST(Context);
     }
-
 private:
     CastCallBack callback_;
     MatchFinder matcher_;
 };
-
 class CStyleCheckerFrontendAction : public ASTFrontendAction {
 public:
     CStyleCheckerFrontendAction() = default;
     void EndSourceFileAction() override {
         rewriter_.getEditBuffer(rewriter_.getSourceMgr().getMainFileID())
-            .write(llvm::outs());
-    }
-
-    std::unique_ptr<ASTConsumer> CreateASTConsumer(CompilerInstance &CI, StringRef /* file */) override {
-        rewriter_.setSourceMgr(CI.getSourceManager(), CI.getLangOpts());
-        return std::make_unique<MyASTConsumer>(rewriter_);
-    }
-
-private:
-    Rewriter rewriter_;
-};
-
-static llvm::cl::OptionCategory CastMatcherCategory("cast-matcher options");
-
-int main(int argc, const char **argv) {
-    CommonOptionsParser OptionsParser(argc, argv, CastMatcherCategory);
-    ClangTool Tool(OptionsParser.getCompilations(),
-            OptionsParser.getSourcePathList());
-
-    return Tool.run(newFrontendActionFactory<CStyleCheckerFrontendAction>().get());
-}
